94ebfba2bd20245bd604013cd5a010db467d1594
Tarzanbot <qrndshtr@gmail.com>
2025-05-15 03:53:40 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || 'KANGO~eAk0TaZb#F4-gLKgKE1Ktbat0qGlGEdWRdL3Kz76ZSuVUCdhMK8I' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~KQUn1axB#xvpq2Pt1XJKKM6bSy4205MStjAopnRXpJPYoZ1LELUo' 
 //Enter your 𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿 session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
